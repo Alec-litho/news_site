fca166bc21082e61d5c389dcdd06996f6c13cc6c
Alec-litho <112494715+Alec-litho@users.noreply.github.com>
2023-08-03 23:55:47 +0300

    y

--- front-end/pages/login.tsx
+++ front-end/pages/login.tsx
@@ -1,8 +1,11 @@
 
 import {useForm, SubmitHandler} from 'react-hook-form'
+import Image from 'next/image';
 import { useAppDispatch, useAppSelector } from '../hooks/reduxCustomHooks';
 import { fetchData } from '../features/authSlice';
 import router from "next/router";
+import Email  from '../public/svg_icons/login/email' 
+import Lock  from '../public/svg_icons/login/lock' 
 type IForm = {
     email: string,
     password: string,
@@ -22,38 +25,72 @@ function Login() {
     }
 
     return (
-        <div className='container'>
-            <form method="post" className="model" onSubmit={handleSubmit(onSubmit)}>
-            <img src="https://i.ibb.co/Bqm8N2r/default-avatar-profile-trendy-style-social-media-user-icon-187599373.jpg" className='profile-picture'></img>
-            <div className="inputContainer">
-                <div className="d-flex">
-                    <svg className="input icon"></svg>
-                    <label>
-                        Email
-                    <input className="email" type="email" placeholder="write email" {...register('email', {required:true})}/>
-                    {errors.email && <p className='error'>email is required</p>}
-                    </label>
-                  
+        <div className='d-flex w-100'>
+            <div className="left_container d-flex justify-content-center align-items-center w-50">
+                <div className="w-75">
+                   <h1>Adventure starts here</h1>
+                   <h3 className='text-white w-50'>Join our lovely community right now</h3>
                 </div>
-                <div className="d-flex">
-                    <svg className="input icon"></svg>
-                    <label>
-                        Password
-                    <input className="password" type="write password" placeholder="Password" {...register("password", {required: true})}/>
-                    {errors.password && <p className='error'>password is required</p>}
-                    </label>
+  
+            </div>
+            <div className="right_container w-50 d-flex justify-content-center align-items-center flex-direction-column">
+                <form method="post" className="model w-50" onSubmit={handleSubmit(onSubmit)}>
+                <h4 className="text-center mb-4">Hello! Welcome back!</h4>
+                <div className="inputContainer ">
+                    <div className="email_container d-flex  position-relative mb-3">
+                        <Email className="icon_A"/>
+                        <label>
+                            Email
+                        <input className="email" type="email" placeholder="Enter your email address" {...register('email', {required:true})}/>
+                        {errors.email && <p className='error'>email is required</p>}
+                        </label>
+                    </div>
+                    <div className="password_container d-flex  position-relative">
+                        <Lock className="icon_A"/>
+                        <label>
+                            Password
+                        <input className="password" type="write password" placeholder="Enter your password" {...register("password", {required: true})}/>
+                        {errors.password && <p className='error'>password is required</p>}
+                        </label>
+                    </div>
+                    
+                </div>
+                <div className='d-flex mb-4 mt-2'>
+                    <div className='d-flex w-100 align-items-center '>
+                        <input type='checkbox'/>
+                        <h6 className='remember color-orange'>remember me</h6>
+                    </div>
+                    <a href="#" className='forgotPass'>reset password?</a>
+                </div>
+                <input className='login' type="submit" ></input>
+                <div className='border d-flex align-items-center justify-content-between mt-4 mb-2'>
+                    <div className='line'></div>
+                    <p>or</p>
+                    <div className='line'></div>
+                </div>
+                <div className='wrapper d-flex w-100 justify-content-center'>
+                <div className=' icons w-50  d-flex justify-content-between'>
+                    <div className='icon'>
+                        <Image src={'/../public/vk.png'} alt="icon" width={50} height={50}/>
+                    </div>
+                    <div className='icon'>
+                        <Image src={'/../public/google.png'} alt="icon" width={40} height={40}/>
+                    </div>
+                    <div className='icon'>
+                        <Image src={'/../public/facebook.png'} alt="icon" width={50} height={50}/>
+                    </div>
+                </div>
+                </div>
+                <div className='wrapper d-flex w-100 justify-content-center'>
+                    <div className='d-flex align-items-center'>
+                        <h6 className='mb-0'>Don't have account?</h6>
+                        <a className="create">Create Account</a>
+                    </div>
                 </div>
                 
+                </form>
             </div>
-            <div className='d-flex'>
-                <div className='d-flex align-items-center'>
-                    <input type='checkbox'/>
-                    <h6 className='remember'>remember me</h6>
-                </div>
-                <a href="#" className='forgotPass'>Forgot password?</a>
-            </div>
-            <input className='login' type="submit" ></input>
-            </form>
+            
         </div>
     )
 }
---
+++ front-end/public/svg_icons/login/lock.jsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+function Lock(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path d="M6 8V6a6 6 0 0112 0v2h-2V6c0-2.206-1.795-4-4-4S8 3.794 8 6v2H6zm15 2v14H3V10h18zm-2 2H5v10h14V12z"
+      fill="#c7592d" />
+    </svg>
+  )
+}
+
+export default Lock
